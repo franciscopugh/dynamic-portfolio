f3484eb52bcb38e439c19bc26a9b09a27254be11
TheDarkWolf <franciscopugh01@gmail.com>
2019-08-04 00:15:51 -0300

    Filtered by categories

--- main.js
+++ main.js
@@ -7,4 +7,16 @@ const grid = new Muuri('.grid', {
 window.addEventListener('load', () => {
     grid.refreshItems().layout();
     document.getElementById('grid').classList.add('uploaded-img');
+
+    const links = document.querySelectorAll('#categories a');
+    links.forEach((e) => {
+        e.addEventListener('click', (event) => {
+            event.preventDefault();
+            links.forEach((link) => link.classList.remove('active') );
+            event.target.classList.add('active');
+
+            const category = event.target.innerHTML.toLowerCase();
+            category === 'all pictures' ? grid.filter('[data-category]') : grid.filter(`[data-category="${category}"]`);        //Conditional If
+        });
+    });
 });
